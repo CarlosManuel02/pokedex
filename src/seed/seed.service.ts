01f0c8b7b621cbf03aaa27ea167f0713be00a14b
CarlosManuel02 <94581164+CarlosManuel02@users.noreply.github.com>
2023-04-26 11:14:19 -0700

    antes de insertar por lote 2

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,30 @@
 import {Injectable} from '@nestjs/common';
 import axios, {AxiosInstance} from "axios";
 import {pokeResponse} from "./interfaces/poke-response.interface";
+import {PokemonService} from "../pokemon/pokemon.service";
 
 @Injectable()
 export class SeedService {
 
     private readonly axios: AxiosInstance = axios;
+    constructor(private readonly pokemonService: PokemonService) {
+    }
 
     async populateDB() {
-        const {data} = await this.axios.get<pokeResponse>('https://pokeapi.co/api/v2/pokemon?offset=500&limit=1000')
+        const {data} = await this.axios.get<pokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0')
 
         data.results.forEach(({name, url})=>{
 
             const segments = url.split('/');
             const no = +segments[segments.length - 2];
 
-
+            this.pokemonService.create({
+                name,
+                no
+            })
         })
 
-        return data.results;
+        return 'Seed executed';
     }
 
 }
