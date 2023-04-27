019f016cfb8f43d060307542eb5e6f4c76d02f37
CarlosManuel02 <94581164+CarlosManuel02@users.noreply.github.com>
2023-04-26 18:49:07 -0700

    pagination

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,6 +4,7 @@ import {UpdatePokemonDto} from './dto/update-pokemon.dto';
 import {Pokemon} from "./entities/pokemon.entity";
 import {isValidObjectId, Model} from "mongoose";
 import {InjectModel} from "@nestjs/mongoose";
+import {PaginationDto} from "../common/dto/pagination.dto";
 
 @Injectable()
 export class PokemonService {
@@ -26,8 +27,15 @@ export class PokemonService {
     }
 
 
-    findAll() {
-        return this.pokemonModel.find();
+    findAll(query: PaginationDto) {
+
+        const {limit = 10, offset = 0} = query;
+
+        return this.pokemonModel.find()
+            .limit(limit)
+            .skip(offset)
+            .sort({no: 1})
+            .select('-__v')
     }
 
     async findOne(term: string) {
